fec0f3b352bb487fde535dd2250e867247f3b3c2
shivam <ssk7860345351@gmail.com>
2023-02-02 01:44:37 +0530

    products

--- src/ProductsList.jsx
+++ src/ProductsList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ProductsService } from "./Services";
+import { BrandsService, ProductsService, CategoriesServices } from "./Services";
 
 let ProductsList = () => {
   // state
@@ -8,8 +8,25 @@ let ProductsList = () => {
 
   useEffect(() => {
     (async () => {
+      // request to brands table
+      let brandsResponse = await BrandsService.fetchBrands();
+      let brandsResponseBody = await brandsResponse.json();
+
+      // request to brands table
+      let categoriesResponse = await CategoriesService.fetchCategories();
+      let categoriesResponseBody = await categoriesResponse.json();
+
+      // request to product table
       let productsResponse = await ProductsService.fetchProducts();
       let productsResponseBody = await productsResponse.json();
+
+      // Set category property into each product
+      productsResponseBody.forEach((product) => {
+        product.category = categoriesServices.getCategoryByCategoryId(
+          categoriesResponseBody,
+          product.categoryId
+        );
+      });
       setProducts(productsResponseBody);
     })();
   }, []);
@@ -29,7 +46,7 @@ let ProductsList = () => {
               type="search"
               placeholder="Search"
               className="form-control"
-              autofocus="autofocus"
+              autoFocus="autofocus"
               value={search}
               onChange={(event) => {
                 setSearch(event.target.value);
@@ -44,8 +61,26 @@ let ProductsList = () => {
           <div className="card-body">
             <table className="table">
               <thead>
-                <tr>#</tr>
+                <tr>
+                  <th>#</th>
+                  <th>Product Name</th>
+                  <th>Price</th>
+                  <th>Brand</th>
+                  <th>Category</th>
+                  <th>Rating</th>
+                </tr>
               </thead>
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product.id}>
+                    <td>{product.id}</td>
+                    <td>{product.productName}</td>
+                    <td>{product.brandId}</td>
+                    <td>{product.categoryId}</td>
+                    <td>{product.rating}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
           </div>
         </div>
